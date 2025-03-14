b3f418ce34ad0718e94386dc0effcaaf814a1c59
minucy-git <minucy@naver.com>
2025-03-14 21:31:21 +0900

    project init

---
+++ src/test/java/com/example/CI_CD_test/CiCdTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CI_CD_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
